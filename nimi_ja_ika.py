40cb352140f2ee82e2ccecec7b46a103b707724e
ottokonttinen <103586741+ottokonttinen@users.noreply.github.com>
2022-08-31 20:06:51 +0300

    Update nimi_ja_ika.py

--- nimi_ja_ika.py
+++ nimi_ja_ika.py
@@ -1,6 +1,3 @@
-# Kirjoita ratkaisu tähän
-from msilib.schema import ODBCTranslator
-# Kirjoita ratkaisu tähän3
 nimi = input('Anna nimi:')
 vuosi = int(input('Anna syntymävuosi:'))
 print(f'Moi {nimi}, olet {2020-vuosi} vuotta vanha vuoden 2020 lopussa')
